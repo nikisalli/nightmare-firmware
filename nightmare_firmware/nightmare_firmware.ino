a4cc8d20a441c9b656e34d73831fb99c83d8d66a
nikisalli <niki.salli@gmail.com>
2020-04-10 23:03:09 +0200

    fixed index out of bound and bad practices

--- nightmare_firmware/nightmare_firmware.ino
+++ nightmare_firmware/nightmare_firmware.ino
@@ -9,23 +9,25 @@ bool servo::write_protection = false;
 
 void setup(){
 	servo::init(SERVO_PIN_TX_ENB, SERVO_PIN_RX_ENB);
-	for(int i=0; i<26; i++){
-		servos[i].id = i+1;
+
+	int x = 0;
+	for(auto& servo : servos){
+		servo.id = ++x;
 	}
 	xTaskCreatePinnedToCore(servo_writer_task, "servo writer task", 10000, NULL, 1, &_servo_writer_task, 1);
 }
 
 void loop(){
-    while(1){
+	while(1){
 		delay(100);
 	}
 }
 
 void servo_writer_task( void * parameter) {
 	while(1) {
-		for (int i = 0; i < 26; i++) {
-			if (servos[i+1].active && !servo::write_protection){
-				servos[i+1].move();
+		for(auto& servo : servos){
+			if (servo.active && !servo::write_protection){
+				servo.move();
 			}
 		}
 	}
